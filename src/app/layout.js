d3f7eb862a75b9f097898fd6b388a74af84f5476
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-05 11:50:37 -0700

    setting up the global css

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='container'>
+           <div className='wrapper'>
+
+               {children}
+           </div>
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+     <Link href='/'>
+       <h1>Welcome to  my website</h1>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos temporibus aut mollitia inventore molestias facere nam eligendi facilis animi consequuntur earum voluptas totam veritatis ea odit quibusdam fugiat, ducimus doloribus.</p>
+     </Link>
+  </div>;
 }
